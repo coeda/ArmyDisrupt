cd4e0f1040a350f29b34f5519edd7c574eeb4782
Bryan Peterson <peter987bp@gmail.com>
2016-09-30 21:12:43 -1000

    update index

---
+++ public/js/donut.js
@@ -0,0 +1,25 @@
+(function (window){
+
+
+window.App = window.App || {};
+
+
+
+let chart = c3.generate({
+    data: {
+        columns: [
+            ['data1', 30],
+            ['data2', 120],
+        ],
+        type : 'donut',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    },
+    donut: {
+        title: "Iris Petal Width"
+
+    }
+});
+
+}(window));
